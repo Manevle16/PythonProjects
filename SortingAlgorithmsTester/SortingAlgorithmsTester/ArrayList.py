9933e19b80dfaf0348de82bb3d34bb6cb0588396
Matthew <Madmatt505@gmail.com>
2018-10-19 04:23:41 -0500

    Sorting method can now be chosen by user before choosing the actual type of sorting needed

--- SortingAlgorithmsTester/SortingAlgorithmsTester/ArrayList.py
+++ SortingAlgorithmsTester/SortingAlgorithmsTester/ArrayList.py
@@ -6,8 +6,53 @@ class ArrayList:
         this.items = []
         rand = Random()
         for i in range(0, numOfItems):
-            this.items.append(rand.randint(0,100))
+            this.items.append(rand.randint(0,500))
 
+    def sortList(this):
+        items = this.items
+        flipped = True
         
+        while(flipped):
+            flipped = False
+            for i in range(0, len(items)-1):
+                for j in range(i + 1, len(items)):
+                    if(items[i] > items[j]):
+                        temp = items[i]
+                        items[i] = items[j]
+                        items[j] = temp
+                        flipped = True
+
+    def reverseSortList(this):
+        items = this.items
+        flipped = True
+        
+        while(flipped):
+            flipped = False
+            for i in range(0, len(items)-1):
+                for j in range(i + 1, len(items)):
+                    if(items[i] < items[j]):
+                        temp = items[i]
+                        items[i] = items[j]
+                        items[j] = temp
+                        flipped = True
     
-    
+    def semiSortList(this):
+        this.sortList()
+        print(this.items)
+        items = this.items
+        rand = Random()
+
+        for i in range(0,len(items)):
+            swap = rand.randint(1,4)
+
+            if(i+swap > len(items)-1):
+                continue
+
+            temp = items[i]
+            items[i] = items[i+swap]
+            items[i+swap] = temp
+            i += rand.randint(0, 4)
+
+            if( i > len(items)-1):
+                break
+
--- SortingAlgorithmsTester/SortingAlgorithmsTester/SortingAlgorithmsTester.py
+++ SortingAlgorithmsTester/SortingAlgorithmsTester/SortingAlgorithmsTester.py
@@ -1,6 +1,40 @@
 from ArrayList import ArrayList
 import math
-numOfItems = int(input("How many items would you like to contain in the list? "))
+while(True):
+    try:
+        numOfItems = int(input("How many items would you like to contain in the list? "))
+        if(numOfItems <= 0 ):
+            print("Number of items must be greater than 0")
+            continue
+        else:
+            break
+    except ValueError:
+        print("Must be of type integer")
+        continue
+
 listOfItems = ArrayList(numOfItems)
 print(listOfItems.items)
+
+while(True):
+    try:
+        print("Would you like the list: \n1. Sorted\n2. Reverse Sorted\n3. Semi-Sorted\n4. Unchanged")
+        selection = int(input("Please enter number corresponding to selection: "))
+        if(selection < 1 or selection > 4):
+            print("Please enter a number from the selection")
+            continue
+        else:
+            break
+    except ValueError:
+        print("Must be of type integer")
+        continue
+
+switch = {1: listOfItems.sortList(),
+          2: listOfItems.reverseSortList(),
+          3: listOfItems.semiSortList(),
+          4: None
+          }
+
+switch[selection]
+print(listOfItems.items)
+
     
