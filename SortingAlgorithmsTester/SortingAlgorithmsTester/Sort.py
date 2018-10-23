6a08c703eeb04c473472115f549589e3ab3c8a17
Matthew <Madmatt505@gmail.com>
2018-10-22 23:47:39 -0500

    Finished quicksort and number of compares, swaps, and time elapsed during sort is shown

--- SortingAlgorithmsTester/SortingAlgorithmsTester/Sort.py
+++ SortingAlgorithmsTester/SortingAlgorithmsTester/Sort.py
@@ -3,38 +3,60 @@ import math
 class Sort:
     """insertionSort"""
 
-    def insertionSort(items):
-        counters = { "compareCounter": 0, "swapCounter": 0 }
+    def __init__(this):
+        this.counters = { "compareCounter": 0, "swapCounter": 0 }
+
+    def insertionSort(this, items):
 
         for i in range(1, len(items)):
             j = i
             while(j > 0):
-                counters["compareCounter"] += 1
+                this.counters["compareCounter"] += 1
                 if(items[j] < items[j - 1]):
-                    counters["swapCounter"] += 1
+                    this.counters["swapCounter"] += 1
                     temp = items[j-1]
                     items[j-1] = items[j]
                     items[j] = temp              
                 j -= 1
-        return counters
     
-    def selectionSort(items):
-        counters = { "compareCounter": 0, "swapCounter": 0 }
+    def selectionSort(this, items):
 
 
         for i in range(len(items)):
             min = math.inf
             minIndex = 0
             for j in range(i, len(items)):
-                counters["compareCounter"] +=1
+                this.counters["compareCounter"] +=1
                 if(items[j] < min):
                     min = items[j]
                     minIndex = j
-            counters["swapCounter"] +=1
+            this.counters["swapCounter"] +=1
             temp = items[i]
             items[i] = items[minIndex]
             items[minIndex] = temp
 
-    def quickSort(items, startInd, endInd):
+    def quickSort(this, items, startInd, endInd):
+        this.counters["compareCounter"] +=1
         if(startInd < endInd):
+             pivot = items[endInd]
+
+             i = startInd - 1
+
+             for j in range(startInd, endInd):
+                 this.counters["compareCounter"] +=1
+                 if(items[j] <= pivot):
+                     this.counters["swapCounter"] +=1
+                     i += 1
+                     temp = items[i]
+                     items[i] = items[j]
+             this.counters["swapCounter"] +=1
+             temp = items[endInd]
+             items[endInd] = items[i+1]
+             items[i+1] = temp
+             partInd = i + 1
+
+             this.quickSort(items, startInd, partInd - 1)
+             this.quickSort(items, partInd + 1, endInd)
+
+
 
