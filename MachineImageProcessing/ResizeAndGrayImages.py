9884b7007d8619784189a3b69a3b64272b8a8d88
Matthew <Madmatt505@gmail.com>
2018-11-17 03:21:32 -0600

    Using backgorund images that weren't anime failed

--- MachineImageProcessing/ResizeAndGrayImages.py
+++ MachineImageProcessing/ResizeAndGrayImages.py
@@ -7,10 +7,13 @@ os.chdir("C:/Users/Manev/github/Manevle16/PythonProjects/MachineImageProcessing"
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
-if not os.path.exists("negative_images/"):
-    os.makedirs("negative_images/")
 
 for file in os.listdir("NegativeImages"):
     img = cv.imread("NegativeImages/" + file, cv.IMREAD_GRAYSCALE)
-    img = cv.resize(img, (50, 50))
-    cv.imwrite("negative_images/"+file, img)
+    img = cv.resize(img, (100, 100))
+    cv.imwrite("NegativeImages/"+file, img)
+
+for file in os.listdir("PositiveImages"):
+    img = cv.imread("PositiveImages/" + file, cv.IMREAD_GRAYSCALE)
+    img = cv.resize(img, (100, 100))
+    cv.imwrite("PositiveImages/"+file, img)
--- MachineImageProcessing/Tester.py
+++ MachineImageProcessing/Tester.py
@@ -5,14 +5,23 @@ import os
 os.chdir("C:/Users/Manev/github/Manevle16/PythonProjects/MachineImageProcessing")
 
 
-cascade = cv.CascadeClassifier('anime_face_fail1_cascade.xml')
-img = cv.imread('test7.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cascade = cv.CascadeClassifier('anime_face_cascade_test2.xml')
+cascade2 = cv.CascadeClassifier('anime_face_fail1_cascade.xml')
+#cascade = cv.CascadeClassifier('lbpcascade_animeface(Not Mine).xml')
 
-faces = cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+for file in os.listdir("TestImages"):
+    img = cv.imread("TestImages/"+file)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-cv.imshow('img', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    faces = cascade.detectMultiScale(gray, 8, 8)
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+#    faces2 = cascade2.detectMultiScale(gray, 1.5, 5)
+#    for (x, y, w, h) in faces2:
+#        cv.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+    img = cv.resize(img, (300, 300))
+    cv.imshow('img', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
