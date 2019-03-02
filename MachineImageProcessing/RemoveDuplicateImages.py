7924689f89cac7a03249e3e8e4d1918d818d4d94
Matthew <Madmatt505@gmail.com>
2019-03-02 17:13:20 -0600

    Delete copies of images

--- MachineImageProcessing/RemoveDuplicateImages.py
+++ MachineImageProcessing/RemoveDuplicateImages.py
@@ -22,13 +22,12 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 # if not os.path.exists("positive_images/"):
 #    os.makedirs("positive_images/")
 
-address = "test_images"
+address = "images"
 print(os.listdir(address))
 i = 1
-for file in os.listdir(address):
-
-    for file2 in os.listdir(address):
-
+files = os.listdir(address)
+for file in files:
+    for file2 in files:
         if(file == file2):
             continue
     #    img1 = Image.open(address + "/" + file)
@@ -46,28 +45,30 @@ for file in os.listdir(address):
         result = cv2.matchTemplate(graySmall, grayLarge, method)
         if(np.min(result) > .01):
             continue
-        print(file + " > " + file2 + " :" + str(np.min(result)))
+        else:
+            print(file + " is same as " + file2)
+            files.remove(file)
+            os.remove(address + "/" + file)
+            break
+#        print(file + " > " + file2 + " :" + str(np.min(result)))
         # We want the minimum squared difference
-        mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+#        mn, _, mnLoc, _ = cv2.minMaxLoc(result)
 
         # Draw the rectangle:
         # Extract the coordinates of our best match
-        MPx, MPy = mnLoc
+#        MPx, MPy = mnLoc
 
         # Step 2: Get the size of the template. This is the same size as the match.
-        trows, tcols = small_image.shape[:2]
+#        trows, tcols = small_image.shape[:2]
 
         # Step 3: Draw the rectangle on large_image
-        cv2.rectangle(large_image, (MPx, MPy), (MPx+tcols, MPy+trows), (0, 0, 255), 2)
+#        cv2.rectangle(large_image, (MPx, MPy), (MPx+tcols, MPy+trows), (0, 0, 255), 2)
 
         # Display the original image with the rectangle around the match.
-        cv2.imshow('output', large_image)
+#        cv2.imshow('output', large_image)
 
         # The image is only displayed if we call this
-        cv2.waitKey(0)
-#        if(equal(img1, img2)):
-#            print(file + " is same as " + file2)
-    #        os.remove(address + "/" + file2)
+#        cv2.waitKey(0)
 
 
 '''faces = face_cascade.detectMultiScale(gray)
